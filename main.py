99d6e07a5335c53dd15b3c1eba466a3a6ee7a04f
cata2 <cappyninja@gmail.com>
2026-07-17 16:49:52 +0300

    Set asteroids to dissapear when they colides with bullets

--- main.py
+++ main.py
@@ -46,6 +46,13 @@ def main():
                 print("Game over!")
                 sys.exit()
         
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
